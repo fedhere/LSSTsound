8a79969e93a9ee5ed3efb8700e54d493ab236b4c
fedhere <fedhere@gmail.com>
2022-07-18 20:57:19 -0400

    adding drums

--- RRhapsodies/main.py
+++ RRhapsodies/main.py
@@ -5,11 +5,18 @@
 
 from rrhapsodies import rr_utils
 from rrhapsodies import configs
+<<<<<<< HEAD
 
 if __name__ == '__main__':
     data, metadata = rr_utils.readdata()
     thisband = 'r'
     object = 43018203 # 615 #2677
+=======
+if __name__ == '__main__':
+    data, metadata = rr_utils.readdata()
+    thisband = 'r'
+    object = 2677
+>>>>>>> 6771a4e280041380882fab8a8b9e1699d633c906
     if False:
         for thisband in ['u', 'g', 'r', 'i', 'z', 'y']:
             print("object:", object, "band", thisband, configs.INSTRUMENTS[thisband],
@@ -20,5 +27,10 @@ if __name__ == '__main__':
                                 key=configs.KEY, save=True, plot=True)
 
     rr_utils.multiSonification(data, object, key=configs.KEY,
+<<<<<<< HEAD
                                drone="gliss", drum='crash cymbal 1',
                                save=True, plot=True)
+=======
+                               drone="gliss", save=True, plot=True)
+
+>>>>>>> 6771a4e280041380882fab8a8b9e1699d633c906
